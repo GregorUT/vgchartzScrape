5bd41bdb80bbd98ccd6ce03ed12f623e7abc80b6
GregorUT <gpsmithut@msn.com>
2016-10-26 01:49:11 -0600

    Update vgchartzfull.py

--- vgchartzfull.py
+++ vgchartzfull.py
@@ -64,6 +64,7 @@ for page in range(1,pages):
 columns = {'rank': rank, 'name': gname, 'platform': platform, 'year': year, 'genre': genre, 'publisher': publisher, 'NA_Sales':sales_na, 'EU_Sales': sales_eu,'JP_Sales': sales_jp,'Other_Sales':sales_ot, 'Global_Sales':sales_gl }
 print rec_count
 df = pd.DataFrame(columns)
+df = df[['Rank','Name','Platform','Year','Genre','Publisher','NA_Sales','EU_Sales','JP_Sales','Other_Sales','Global_Sales']]
 del df.index.name
 df.to_csv("vgsales.csv",sep=",",encoding='utf-8')
 
