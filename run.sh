7f5719e7cefaaef830d6b16688ea22670a319ed2
Manuel Eusebio de Paz Carmona <pelirrojo@users.noreply.github.com>
2020-03-31 18:19:27 +0200

    Improve script output

--- run.sh
+++ run.sh
@@ -5,5 +5,9 @@ pip --version >/dev/null 2>&1 || { echo >&2 "I require pip utility but it's not 
 
 clear
 
+echo "\nInstalling deps... "
 pip install -r requirements.txt
+
+echo "\nStart crawling... (remember a crawler is the friend nobody likes)"
 python vgchartz-full-crawler.py
+
